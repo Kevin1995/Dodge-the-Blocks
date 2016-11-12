87db3374498dd880a871da8cf2f74a555e97fbc1
Kevin1995 <kevinkehoe18@gmail.com>
2016-11-12 00:43:33 +0000

    Comments added

--- Dodge the Blocks/Assets/Block.cs
+++ Dodge the Blocks/Assets/Block.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Block : MonoBehaviour {
 
-	// Update is called once per frame
+	// Destroys Blocks after they go past -2 on the y axis
 	void Update () {
 
         if (transform.position.y < -2f)
--- Dodge the Blocks/Assets/GameManager.cs
+++ Dodge the Blocks/Assets/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
+    // Reloads scene
 	public void EndGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
