debedf06039a039ab69bfa7170c0e83e57332a7c
Kevin1995 <kevinkehoe18@gmail.com>
2016-11-12 00:41:19 +0000

    Added destruction of blocks and restart if game

---
+++ Dodge the Blocks/Assets/Block.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+
+        if (transform.position.y < -2f)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
---
+++ Dodge the Blocks/Assets/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public void EndGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
